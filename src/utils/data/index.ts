96ed74772aef5b137c09ad9ad81046a45a73eb9d
u76ner <muramatu.kosuke.0610@gmail.com>
2021-04-26 08:57:11 +0900

    ♻️

--- src/utils/data/index.ts
+++ src/utils/data/index.ts
@@ -1,17 +1,15 @@
 import { op31JojoSPA } from "./op31JojoSPA";
 
-type Section = {
-  bpm: number;
-  // ある程度長く続くセクションかどうか
-  isMain: boolean;
-  // ギアチェンしやすいか
-  // difficult: 厳しい、notBad: 数ノーツ捨てればできる、easy: 余裕
-  isEasyToOperate: "difficult" | "notBad" | "easy";
-};
-
 export type Song = {
   title: string;
-  sections: Section[];
+  sections: {
+    bpm: number;
+    // ある程度長く続くセクションかどうか
+    isMain: boolean;
+    // ギアチェンしやすいか
+    // difficult: 厳しい、notBad: 数ノーツ捨てればできる、easy: 余裕
+    isEasyToOperate: "difficult" | "notBad" | "easy";
+  }[];
 };
 
 export const songs: Song[] = [
